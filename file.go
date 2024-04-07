e9b9d9bfd582dbce77c15f6f64a19f7ee9bd2d14
yanyanlinyu <132678513+yanyanlinyu@users.noreply.github.com>
2024-04-07 18:43:44 +0800

    Update file.go
    edit

--- file.go
+++ file.go
@@ -1,7 +1,13 @@
 package merkledag
 
 const (
-	FILE = iota
+	AddressLength = 20
+	AddressSize   = 20
+	BucketSize    = 3
+	NumNodes      = 100
+	NumPeers      = 100
+	NumBuckets    = 5
+	FILE          = iota
 	DIR
 )
 
@@ -27,4 +33,30 @@ type DirIterator interface {
 	Next() bool
 
 	Node() Node
+}
+
+// ExampleNode 是 Node 接口的一个示例实现
+type ExampleNode struct {
+	ID   string
+	size uint64
+}
+
+type Peer struct {
+	ID   string
+	Size uint64
+	DHT  DHT
+}
+
+type DHT struct {
+	Buckets []Bucket
+}
+
+// Bucket 表示 Kademlia DHT 中的一个桶
+type Bucket struct {
+	Peers []Peer
+}
+
+// KBucket 表示 Kademlia DHT 中的 K_Bucket 算法
+type KBucket struct {
+	Buckets []Bucket
 }
